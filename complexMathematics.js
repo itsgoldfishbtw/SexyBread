fe65493377f26c4ea4bd6b151db34f25424a48a0
itsgoldfishbtw <67710599+itsgoldfishbtw@users.noreply.github.com>
2020-07-01 22:18:08 +0200

    Create complexMathematics.js

---
+++ complexMathematics.js
@@ -0,0 +1,19 @@
+function complexMathematics(){
+	var val=Math.abs(hash(document.getElementById("name").value.replace(/_/g,"a").toLowerCase()));
+  var endVal=Math.abs(parseInt((val/(10**(val.toString().length-1)))*10));
+  endVal=((endVal<50)?(endVal+30):endVal);
+  console.log(val.toString().length)
+  console.log(val)
+  console.log(val/(10**(val.toString().length-1)))
+	document.getElementById("sexiness").innerHTML=endVal.toString();
+}
+
+function hash(string){
+  var hash = 0, i, chr;
+  for (i = 0; i < string.length; i++) {
+    chr   = string.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0;
+  }
+  return hash;
+}
